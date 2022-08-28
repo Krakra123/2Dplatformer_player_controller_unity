745d4ca43a4f9113a49c62ad5efba1bfc3c48bc3
Krakra <69046270+Krakra123@users.noreply.github.com>
2022-08-28 21:24:53 +0700

    other #1

--- PlayerController.cs
+++ PlayerController.cs
@@ -7,16 +7,15 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D thisRigidbody;
     BoxCollider2D thisCollider;
 
-    private float desiredtVelocityX;
-
-    [Header("Ground")]
+    [Header("Input")]
     public string movementInputAxis;
     public string jumpInputButton;
-    [HideInInspector] public int inputDirectionX;
-    [HideInInspector] public bool inputJump;
+    private int inputDirectionX;
+    private bool inputJump;
+    private bool inputJumpHold;
 
     [Header("Ground")]
-    public LayerMask groundMask;
+    public LayerMask groundLayerMask;
     [HideInInspector] public bool onGround;
 
     [Header("Running")]
@@ -31,7 +30,20 @@ public class PlayerController : MonoBehaviour
     [Range(1f, 10f)] public float onAirDownGravityScale;
     [Range(.1f, 50f)] public float onAirSpeedAcceleration;
     [Range(.1f, 50f)] public float onAirSpeedControl;
-    [Range(0f, 5f)] public float variableJumpScale;
+    [Range(0f, 5f)] public float variableJumpCutOff;
+    private bool variableJumpDragDownStart = false;
+
+    [Header("Other")]
+    [Range(0f, .3f)] public float coyoteTime;
+    private float coyoteTimeCounter;
+    [Range(0f, .3f)] public float jumpBuffer;
+    private float bufferTimeCounter;
+    private bool earlyJumpInput = false;
+    public float terminalVelocity;
+
+    private bool isJumping;
+
+    private float desiredtVelocityX;
 
     private float movementAcceleration;
     private float movementDeceleration;
@@ -46,11 +58,12 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         inputDirectionX = (int)Input.GetAxisRaw(movementInputAxis);
-        inputJump = Input.GetButton(jumpInputButton);
+        inputJump = Input.GetButtonDown(jumpInputButton);
+        inputJumpHold = Input.GetButton(jumpInputButton);
 
         OnGroundCheck();
 
-        Jumping();
+        JumpingCalculation();
 
         desiredtVelocityX = inputDirectionX * runningMaxSpeed;
     }
@@ -59,19 +72,19 @@ public class PlayerController : MonoBehaviour
     {
         Movement();
 
-        Gravity();
+        GravityCalculation();
     }
 
     private void OnGroundCheck()
     {
-        float groundCheckThick = .1f;
+        float groundCheckThick = Physics2D.defaultContactOffset * 2f;
         float groundCheckOffset = .1f;
 
         Vector2[] groundCheckCorner = new Vector2[2];
         groundCheckCorner[0] = transform.position + (Vector3)thisCollider.offset + new Vector3(-thisCollider.size.x / 2 + groundCheckOffset, -thisCollider.size.y / 2 - groundCheckOffset);
         groundCheckCorner[1] = transform.position + (Vector3)thisCollider.offset + new Vector3(+thisCollider.size.x / 2 - groundCheckOffset, -thisCollider.size.y / 2 - groundCheckOffset - groundCheckThick);
 
-        if (Physics2D.OverlapArea(groundCheckCorner[0], groundCheckCorner[1], groundMask))
+        if (Physics2D.OverlapArea(groundCheckCorner[0], groundCheckCorner[1], groundLayerMask))
         {
             onGround = true;
         }
@@ -91,35 +104,71 @@ public class PlayerController : MonoBehaviour
         thisRigidbody.velocity = _velocity;
     }
 
-    private void Jumping()
+    private void JumpingCalculation()
     {
         Vector2 _velocity = thisRigidbody.velocity;
 
-        bool variableJumpReset = true;
-
-        if (onGround)
+        if (!onGround)
         {
-            variableJumpReset = true;
+            coyoteTimeCounter -= Time.deltaTime;
+        }
 
-            if (inputJump)
+        if (inputJump && coyoteTimeCounter < 0f && _velocity.y <= 0f) earlyJumpInput = true;
+        if (earlyJumpInput)
+        {
+            bufferTimeCounter -= Time.deltaTime;
+        }
+
+        if (isJumping)
+        {
+            if (!inputJumpHold && _velocity.y > 0 && !variableJumpDragDownStart) variableJumpDragDownStart = true;
+            if (variableJumpDragDownStart)
             {
-                _velocity.y = jumpHeight;
+                _velocity.y += variableJumpCutOff * gravityScale * Physics2D.gravity.y * Time.deltaTime;
+            }
+
+            if (_velocity.y <= 0f && onGround)
+            {
+                isJumping = false;
             }
         }
-        else
-        {
-            if (!inputJump && variableJumpReset) variableJumpReset = false;
 
-            if (!variableJumpReset)
+        if (inputJump)
+        {
+            if (coyoteTimeCounter >= 0f && !isJumping)
             {
-                _velocity.y += variableJumpScale * gravityScale * Physics2D.gravity.y * Time.deltaTime;
+                Jump(ref _velocity);
             }
+        }
+
+        if (earlyJumpInput)
+        {
+            if (onGround && bufferTimeCounter >= 0f)
+            {
+                earlyJumpInput = false;
+
+                Jump(ref _velocity);
+            }
+        }
+        if (onGround)
+        {
+            variableJumpDragDownStart = false;
+            coyoteTimeCounter = coyoteTime;
+            bufferTimeCounter = jumpBuffer;
+            earlyJumpInput = false;
         }
 
         thisRigidbody.velocity = _velocity;
     }
 
-    private void Gravity()
+    void Jump(ref Vector2 velocity)
+    {
+        isJumping = true;
+
+        velocity.y = jumpHeight;
+    }
+
+    private void GravityCalculation()
     {
         if (!onGround && thisRigidbody.velocity.y < 0)
         {
